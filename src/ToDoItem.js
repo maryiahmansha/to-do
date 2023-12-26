2dd14b7e8060e2204373be5027885e15a0cdd782
Maryiah Mansha <marymansha15@gmail.com>
2023-12-26 20:21:23 +0000

    cross out functionality implemented

--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -1,12 +1,12 @@
 const ToDoItem = ({ completed, id, title, toggleTodo, deleteTodo }) => {
     return (
-        <li key={id}>
-            <label className='checkbox'>
+        <li key={id} >
+            <label className='checkbox font-extrabold'>
                 <input type='checkbox' checked={completed} onChange={e => toggleTodo(id, e.target.checked)} />
-                {title}
+                <span style={{ fontWeight: 'bold', textDecoration: completed ? 'line-through' : 'none' }}>{title}</span>
             </label>
             <button className="btn-delete" onClick={() => deleteTodo(id)}>Delete</button>
-        </li>
+        </li >
     );
 }
 
